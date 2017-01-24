157da93438d3499ab52e1d5c72377a9c9ea5985b
Joey Caughey <joey.caughey@gmail.com>
2017-01-24 16:45:37 -0500

    Update skelleton.model-api.js

--- skelleton.model-api.js
+++ skelleton.model-api.js
@@ -1,35 +1,40 @@
-Skelleton.Model =  {
-	all: function(callback) {
-		var self = this;
-		Skelleton.API.get(self.name, function(request){
-			if (request.success) {
-				callback(request.response);
-			}
-		});
-	},
-	get: function(parameters, callback) {
-		var self = this;
-		Skelleton.API.get(self.name, parameters, function(request){
-			if (request.success) {
-				callback(request.response);
-			}
-		});
-	},
-	save: function(parameters, callback) {
-		var self = this;
-		var method = (parameters.id) ? "update" : "insert";
 
-		Skelleton.API[method](self.name, parameters, function(request){
-			if (request.success) {
-				callback(request.response);
-			}
-		});
-	},
-	delete: function(parameters, callback) {
-		Skelleton.API["delete"](self.name, parameters, function(request){
-			if (request.success) {
-				callback(request.response);
-			}
-		});
-	}
+class Model {
+    constructor(schema, table) {
+        this._schema = schema;
+        this.table = table;
+    }
+    init() {
+    	var self = this;
+    	self.all(function(request){
+    		//Content.loop("[data-content-loop="+self.table+"]", request.response);
+    	});
+    }
+    all(callback) {
+        var self = this;
+        Skelleton.API.get(self.table, function(request) {
+            callback(request);
+        });
+    }
+    get(parameters, callback) {
+        var self = this;
+
+        Skelleton.API.get(self.table, parameters, function(request) {
+            callback(request);
+        });
+    }
+    save(parameters, callback) {
+        var self = this;
+        var method = (parameters.id) ? "update" : "insert";
+
+        Skelleton.API[method](self.table, parameters, function(request) {
+            callback(request);
+        });
+    }
+    delete(parameters, callback) {
+        var self = this;
+        Skelleton.API["delete"](self.table, parameters, function(request) {
+            callback(request);
+        });
+    }
 }
